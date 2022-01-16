1b2013f55ff9679473e90339ab9687cc6e89b8cb
ahmedraed0097 <ahmedraed0097@gmail.com>
2022-01-16 21:57:47 +0300

    first design of footer section

---
+++ components/footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row no-gutters class="footer-wrapper-row">
+      <v-col>
+          <div class="first-section">
+              <p class="first-section-title">
+                  MY BLOG
+              </p>
+          </div>
+          <div class="social-media-icons-container">
+              <i class="instagram">mdi mdi-instagram</i>
+              <i class="facebook">mdi mdi-facebbok</i>
+              <i class="twitter">mdi mdi-twitter</i>
+          </div>
+      </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.footer-wrapper-row{
+    background: rgb(46, 45, 45);
+    min-height: 260px;
+    color: #fff;
+}
+</style>
