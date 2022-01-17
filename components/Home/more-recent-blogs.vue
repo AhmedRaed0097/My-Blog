4a665004552671aae2a5a80501608d26fea26848
ahmedraed0097 <ahmedraed0097@gmail.com>
2022-01-17 21:52:41 +0300

    change component name

--- components/Home/more-recent-blogs.vue
+++ components/Home/more-recent-blogs.vue
@@ -1,20 +1,20 @@
 <template>
     <v-row>
       <v-col cols="12" style="padding: 20px !important">
-        <h2 style="text-align: center">More Recent Posts</h2>
+        <h2 style="text-align: center">More Recent Blogs</h2>
       </v-col>
       <v-col v-for="i in 4" :key="i"  xs="12" sm="6" md="6" lg="3" class="card-col">
-        <v-card width="400" class="post-card">
-          <div class="post-image">
+        <v-card width="400" class="blog-card">
+          <div class="blog-image">
             <img
               width="100%"
               src="~/assets/images/header-image.jpg"
-              alt="post image"
+              alt="blog image"
             />
           </div>
-          <div class="post-title">Lorem Ipsum Dolor Sit Aemt Facere.</div>
-          <div class="view-post-conatiner">
-            <v-btn text class="view-post-btn">VIEW THE POST</v-btn>
+          <div class="blog-title">Lorem Ipsum Dolor Sit Aemt Facere.</div>
+          <div class="view-blog-conatiner">
+            <v-btn text class="view-blog-btn">VIEW THE blog</v-btn>
           </div>
         </v-card>
       </v-col>
@@ -32,18 +32,18 @@ export default {}
   justify-content: center;
 }
 
-.post-card {
+.blog-card {
   border-radius: 10px !important;
-  .post-image {
+  .blog-image {
     img {
       border-radius: 10px 10px 0 0;
     }
   }
-  .post-title {
+  .blog-title {
     height: 110px;
     padding: 5px 8px !important;
   }
-  .view-post-conatiner {
+  .view-blog-conatiner {
     padding: 8px !important;
     
   }
