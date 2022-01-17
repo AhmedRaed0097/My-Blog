89ffd04bcbb84d09ebf91fe67efb28c4c57cd38a
ahmedraed0097 <ahmedraed0097@gmail.com>
2022-01-17 21:53:48 +0300

    use quill plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,10 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    { src: '~plugins/quill.js', ssr: false }
+
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
