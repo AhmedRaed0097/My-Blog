d5da1a1bda075d836fe9ea54009113490261c911
ahmedraed0097 <ahmedraed0097@gmail.com>
2022-01-26 09:41:53 +0300

    change defalut editor alignment

--- components/Editor.vue
+++ components/Editor.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="container">
     <no-ssr placeholder="Loading Your Editor...">
+      <p v-show="disabledEditor === true">LOADING</p>
       <vue-editor
         id="editor"
         v-model="content"
         useCustomImageHandler
         @imageAdded="handleImageAdded"
+        placeholder="أدخل محتوى المقال هنا"
+        v-show="disabledEditor === false"
       />
     </no-ssr>
     <div class="blog-actions">
@@ -16,12 +19,52 @@
 </template>
  <script>
 export default {
-  data() {
-    return {
-      content: '',
+  props:{
+    content:{
+      type:String,
+      default: null
     }
   },
+  data() {
+    return {
+      
+      disabledEditor: true,
+    }
+  },
+  created() {
+    if (process.client) {
+      setTimeout(() => {
+        const alignLeftButton = window.document
+          .getElementsByClassName('ql-align')
+          .item(0)
+        const alignRightButton = window.document
+          .getElementsByClassName('ql-align')
+          .item(2)
+        const editor = window.document
+          .getElementsByClassName('ql-editor')
+          .item(0)
+        let pressForFirsttime = false
+        console.log('editor ', editor)
+        editor.addEventListener('click', function () {
+          if (pressForFirsttime === false) {
+            alignLeftButton.classList.remove('ql-active')
+            alignRightButton.classList.add('ql-active')
+          }
+        })
+        editor.classList.add('custom-ql-editor')
+        console.log('editor', editor)
 
+        console.log('alignLeftButton.classList', alignLeftButton.classList)
+        alignLeftButton.addEventListener('click', function () {
+          alignLeftButton.classList.toggle('ql-active')
+
+          editor.classList.remove('custom-ql-editor')
+          pressForFirsttime = true
+        })
+        this.disabledEditor = false
+      }, 2000)
+    }
+  },
   methods: {
     handleImageAdded(file, Editor, cursorLocation, resetUploader) {
       console.log('img', file)
@@ -63,13 +106,15 @@ export default {
   padding: 10px 30px !important;
 }
 
-
 .ql-editor {
   min-height: 350px !important;
   height: 350px !important;
   overflow-y: scroll;
   font-size: 16px;
   padding: 30px !important;
+}
+.custom-ql-editor {
+  text-align: right !important;
 }
 .blog-actions {
   display: flex;
